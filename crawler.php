889b82c4e680e7e907791700468d9593b9d271da
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-10-10 19:16:36 +0630

    Create crawler.php

---
+++ crawler.php
@@ -0,0 +1,45 @@
+<?php
+
+    $source = [
+        'https://t.me/s/warpplus',
+        'https://t.me/s/warppluscn',
+        'https://t.me/s/warpPlusHome',
+        'https://t.me/s/warp_veyke',
+    ];
+
+    $keys = [];
+    $pattern = '/<code>([A-Za-z0-9-]+)<\/code>/';
+    foreach($source as $url) {
+        $getData = file_get_contents($url);
+        if ( preg_match_all($pattern, $getData, $matches) ) {
+            $keys = array_merge($keys, $matches[1]);
+        }
+    }
+
+    $keys = array_unique($keys);
+    if ( count($keys) > 0 ) {
+
+        $x = 0;
+        $full = "";
+        foreach($keys as $key) {
+            if ( $x >= 100 ) {
+                break;
+            }
+            $full .= $key.( $key !== end($keys) ? "\n" : "");
+            $x++;
+        }
+        file_put_contents("plus/full.txt", $full);
+
+        $i = 0;
+        $lite = "";
+        shuffle($keys);
+        foreach($keys as $key) {
+            if ( $i >= 15 ) {
+                break;
+            }
+            $lite .= $key.( $key !== end($keys) ? "\n" : "");
+            $i++;
+        }
+        file_put_contents("plus/lite.txt", $lite);
+
+    }
